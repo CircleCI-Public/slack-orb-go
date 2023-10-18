0c39d9f0be3c63157dd0c23fa3f0a5940c6bdcde
Eric Ribeiro <ericribeiro@outlook.com.br>
2023-10-18 07:54:29 -0700

    refactor: replace `id` with `description` in the tests

--- src/scripts/config/config_test.go
+++ src/scripts/config/config_test.go
@@ -8,67 +8,67 @@ import (
 func TestExpandEnvVariables(t *testing.T) {
 	tests := []struct {
 		configVar   string
+		description string
 		envVars     map[string]string
 		expectedErr string
 		expectedVal string
 		fieldName   string
-		id          string
 	}{
 		{
 			// This test case checks the basic functionality of variable expansion.
 			// An environment variable "TEST_VAR" is set, and it is expected that
 			// the variable in the configuration gets expanded to the value of "TEST_VAR".
 			configVar:   "${TEST_VAR}",
+			description: "BasicVariableExpansion",
 			envVars:     map[string]string{"TEST_VAR": "value"},
 			expectedErr: "",
 			expectedVal: "value",
 			fieldName:   "AccessToken",
-			id:          "1",
 		},
 		{
 			// This test case checks whether a suffix can be successfully appended
 			// to the value of an environment variable after it gets expanded.
 			configVar:   "${TEST_VAR}_suffix",
+			description: "VariableWithSuffix",
 			envVars:     map[string]string{"TEST_VAR": "value"},
 			expectedErr: "",
 			expectedVal: "value_suffix",
 			fieldName:   "AccessToken",
-			id:          "2",
 		},
 		{
 			// This test case checks whether two environment variables can be
 			// concatenated successfully after they get expanded.
 			configVar:   "${TEST_VAR}_${ANOTHER_VAR}",
+			description: "ConcatenateTwoVariables",
 			envVars:     map[string]string{"TEST_VAR": "value", "ANOTHER_VAR": "another_value"},
 			expectedErr: "",
 			expectedVal: "value_another_value",
 			fieldName:   "AccessToken",
-			id:          "3",
 		},
 		{
 			// This test case checks whether the default value is used when the environment
 			// variable is set but empty. The default value "default_value" should be used.
 			configVar:   "${TEST_VAR:-default_value}",
+			description: "DefaultForEmptyVariable",
 			envVars:     map[string]string{"TEST_VAR": ""},
 			expectedErr: "",
 			expectedVal: "default_value",
 			fieldName:   "AccessToken",
-			id:          "4",
 		},
 		{
 			// This test case checks the behavior when an environment variable is unset.
 			// The configuration should have an empty value as "UNSET_VAR" is not set.
 			configVar:   "${UNSET_VAR}",
+			description: "UnsetVariable",
 			envVars:     map[string]string{},
 			expectedErr: "",
 			expectedVal: "",
 			fieldName:   "AccessToken",
-			id:          "5",
 		},
 	}
 
 	for _, test := range tests {
-		t.Run(test.id, func(t *testing.T) {
+		t.Run(test.description, func(t *testing.T) {
 			// Setting environment variables
 			for varName, val := range test.envVars {
 				os.Setenv(varName, val)
@@ -103,31 +103,31 @@ func TestExpandEnvVariables(t *testing.T) {
 func TestValidate(t *testing.T) {
 	tests := []struct {
 		config      *Config
+		description string
 		expectedErr string // This holds the name of the field expected to error
-		id          string
 	}{
 		{
 			// This test case checks the behavior when the access token is missing.
 			config:      &Config{AccessToken: "", ChannelsStr: "channel"},
+			description: "MissingAccessToken",
 			expectedErr: "SLACK_ACCESS_TOKEN",
-			id:          "1",
 		},
 		{
 			// This test case checks the behavior when the channel string is missing.
 			config:      &Config{AccessToken: "token", ChannelsStr: ""},
+			description: "MissingChannelString",
 			expectedErr: "SLACK_PARAM_CHANNEL",
-			id:          "2",
 		},
 		{
 			// This test case checks the behavior when nothing is missing.
 			config:      &Config{AccessToken: "token", ChannelsStr: "channel"},
+			description: "ValidConfig",
 			expectedErr: "",
-			id:          "3",
 		},
 	}
 
 	for _, test := range tests {
-		t.Run(test.id, func(t *testing.T) {
+		t.Run(test.description, func(t *testing.T) {
 			err := test.config.Validate()
 
 			if err != nil {
@@ -148,40 +148,40 @@ func TestValidate(t *testing.T) {
 
 func TestLoadEnvFromFile(t *testing.T) {
 	tests := []struct {
+		description string
 		envVarName  string
 		envVarValue string
 		expectedErr bool
 		filePath    string
-		id          string
 	}{
 		{
 			// This test case checks the behavior when the file does not exist.
+			description: "FileDoesNotExist",
 			filePath:    "/path/that/does/not/exist",
 			envVarName:  "",
 			envVarValue: "",
 			expectedErr: false,
-			id:          "1",
 		},
 		{
 			// This test case checks the successful loading of environment variables from a file.
+			description: "ValidFile",
 			envVarName:  "TEST_VAR",
 			envVarValue: "potato",
 			expectedErr: false,
 			filePath:    "testdata/valid_env_file",
-			id:          "2",
 		},
 		{
 			// This test case checks the behavior when the file is invalid.
+			description: "InvalidFile",
 			envVarName:  "TEST_VAR",
 			envVarValue: "potato",
 			expectedErr: true,
 			filePath:    "testdata/invalid_env_file",
-			id:          "3",
 		},
 	}
 
 	for _, test := range tests {
-		t.Run(test.id, func(t *testing.T) {
+		t.Run(test.description, func(t *testing.T) {
 
 			err := LoadEnvFromFile(test.filePath)
 
