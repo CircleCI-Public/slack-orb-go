9b8ef9b0bd55705073be49f2b797abfbf3892567
Brian Vu <emailbrian.vu@gmail.com>
2023-10-19 14:51:57 -0700

    fix: add int return value to function

--- src/scripts/httputils/httputils.go
+++ src/scripts/httputils/httputils.go
@@ -10,7 +10,7 @@ import (
 func SendHTTPRequest(method, url, body string, headers map[string]string) (string, int, error) {
 	req, err := http.NewRequest(method, url, bytes.NewBuffer([]byte(body)))
 	if err != nil {
-		return "", fmt.Errorf("Error creating request: %v", err)
+		return "", 0, fmt.Errorf("error creating request: %v", err)
 	}
 	for key, value := range headers {
 		req.Header.Set(key, value)
@@ -18,13 +18,13 @@ func SendHTTPRequest(method, url, body string, headers map[string]string) (strin
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		return "", fmt.Errorf("Error sending request: %v", err)
+		return "", 0, fmt.Errorf("error sending request: %v", err)
 	}
 	defer resp.Body.Close()
 
 	respBody, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return "", resp.StatusCode, fmt.Errorf("Error reading response: %v", err)
+		return "", resp.StatusCode, fmt.Errorf("error reading response: %v", err)
 	}
 
 	return string(respBody), resp.StatusCode, nil
