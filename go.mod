f8d4e1dbd250c17801e9b156116a27a285c6127a
Brian Vu <emailbrian.vu@gmail.com>
2023-10-19 09:11:45 -0700

    refactor: add slack package to main

--- go.mod
+++ go.mod
@@ -4,4 +4,16 @@ go 1.21.1
 
 require github.com/joho/godotenv v1.5.1
 
-require github.com/a8m/envsubst v1.4.2
+require (
+	github.com/a8m/envsubst v1.4.2
+	github.com/stretchr/testify v1.8.4
+	gotest.tools/v3 v3.5.1
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/jarcoal/httpmock v1.3.1
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- src/scripts/main.go
+++ src/scripts/main.go
@@ -8,9 +8,7 @@ import (
 	"strings"
 
 	"github.com/CircleCI-Public/slack-orb-go/src/scripts/config"
-	"github.com/CircleCI-Public/slack-orb-go/src/scripts/httputils"
-	"github.com/CircleCI-Public/slack-orb-go/src/scripts/jsonutils"
-	"github.com/CircleCI-Public/slack-orb-go/src/scripts/stringutils"
+	"github.com/CircleCI-Public/slack-orb-go/src/scripts/slack"
 )
 
 func main() {
@@ -50,85 +48,45 @@ func main() {
 		}
 	}
 
-	// Exit if the job status does not match the message send event and the message send event is not set to "always"
-	if !stringutils.IsEventMatchingStatus(conf.EventToSendMessage, conf.JobStatus) {
-		message := fmt.Sprintf("The job status %q does not match the status set to send alerts %q.", conf.JobStatus, conf.EventToSendMessage)
+	invertMatch, _ := strconv.ParseBool(conf.InvertMatchStr)
+	ignoreErrors, _ := strconv.ParseBool(conf.IgnoreErrorsStr)
+	channels := strings.Split(conf.ChannelsStr, ",")
+
+	slackNotification := slack.Notification{
+		Status:                   conf.JobStatus,
+		Branch:                   conf.JobBranch,
+		Tag:                      conf.JobTag,
+		Event:                    conf.EventToSendMessage,
+		BranchPattern:            conf.BranchPattern,
+		TagPattern:               conf.TagPattern,
+		InvertMatch:              invertMatch,
+		InlineTemplate:           conf.InlineTemplate,
+		EnvVarContainingTemplate: conf.EnvVarContainingTemplate,
+	}
+
+	modifiedJSON := slackNotification.BuildMessageBody()
+	
+	slackMessage := slack.Message{
+		Template: modifiedJSON,
+		AccessToken: conf.AccessToken,
+		IgnoreErrors: ignoreErrors,
+		Channels: channels,
+	}
+
+	if !slackNotification.IsEventMatchingStatus() {
+		message := fmt.Sprintf("The job status %q does not match the status set to send alerts %q.", slackNotification.Status, slackNotification.Event)
 		fmt.Println(message)
 		fmt.Println("Exiting without posting to Slack...")
 		os.Exit(0)
 	}
 
-	// Check if the branch and tag match their respective patterns and parse the invert match parameter
-	branchMatches, err := stringutils.IsPatternMatchingString(conf.BranchPattern, conf.JobBranch)
-	if err != nil {
-		log.Fatal("Error parsing the branch pattern:", err)
-	}
-	tagMatches, err := stringutils.IsPatternMatchingString(conf.TagPattern, conf.JobTag)
-	if err != nil {
-		log.Fatal("Error parsing the tag pattern:", err)
-	}
-	invertMatch, _ := strconv.ParseBool(conf.InvertMatchStr)
-	if !stringutils.IsPostConditionMet(branchMatches, tagMatches, invertMatch) {
+	if !slackNotification.IsPostConditionMet() {
 		fmt.Println("The post condition is not met. Neither the branch nor the tag matches the pattern or the match is inverted.")
 		fmt.Println("Exiting without posting to Slack...")
 		os.Exit(0)
 	}
 
-	// Build the message body
-	template, err := jsonutils.DetermineTemplate(conf.InlineTemplate, conf.JobStatus, conf.EnvVarContainingTemplate)
-	if err != nil {
-		log.Fatalf("%v", err)
-	}
-	if template == "" {
-		log.Fatalf("the template %q is empty. Exiting without posting to Slack...", template)
-	}
-
-	// Expand environment variables in the template
-	templateWithExpandedVars, err := jsonutils.ApplyFunctionToJSON(template, jsonutils.ExpandEnvVarsInInterface)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Add a "channel" property with a nested "myChannel" property
-	modifiedJSON, err := jsonutils.ApplyFunctionToJSON(templateWithExpandedVars, jsonutils.AddRootProperty("channel", "my_channel"))
-	if err != nil {
-		log.Fatalf("%v", err)
-	}
-
-	ignoreErrors, _ := strconv.ParseBool(conf.IgnoreErrorsStr)
-	channels := strings.Split(conf.ChannelsStr, ",")
-	for _, channel := range channels {
-		// Add a "channel" property with the current channel
-		jsonWithChannel, err := jsonutils.ApplyFunctionToJSON(modifiedJSON, jsonutils.AddRootProperty("channel", channel))
-		if err != nil {
-			log.Fatalf("%v", err)
-		}
-		fmt.Printf("Posting the following JSON to Slack:\n%s\n", jsonWithChannel)
-
-		// Post the message to Slack
-		headers := map[string]string{
-			"Content-Type":  "application/json",
-			"Authorization": "Bearer " + conf.AccessToken,
-		}
-		response, err := httputils.SendHTTPRequest("POST", "https://slack.com/api/chat.postMessage", jsonWithChannel, headers)
-		if err != nil {
-			log.Fatalf("Error posting to Slack: %v", err)
-		}
-		fmt.Printf("Slack API response:\n%s\n", response)
-
-		// Check if the Slack API returned an error message
-		errorMsg, err := jsonutils.ApplyFunctionToJSON(response, jsonutils.ExtractRootProperty("error"))
-		if err != nil {
-			log.Fatalf("Error extracting error message: %v", err)
-		}
-
-		// Exit if the Slack API returned an error message and the ignore errors parameter is not set to true
-		if errorMsg != "" {
-			fmt.Printf("Slack API returned an error message:\n%s", errorMsg)
-			fmt.Println("\n\nView the Setup Guide: https://github.com/CircleCI-Public/slack-orb/wiki/Setup")
-			if !ignoreErrors {
-				os.Exit(1)
-			}
-		}
+	for _, channel := range slackMessage.Channels {
+		slackMessage.PostMessage(channel)
 	}
 }
