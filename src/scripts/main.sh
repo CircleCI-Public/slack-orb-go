513c27115e90d3cd393a1ddc8b69d39ea43a365d
Eric Ribeiro <ericribeiro@outlook.com.br>
2023-10-30 17:36:36 -0700

    ci: fix wget command

--- src/scripts/main.sh
+++ src/scripts/main.sh
@@ -61,8 +61,7 @@ determine_release_latest_version() {
   if [ "$3" = "curl" ]; then
     LATEST_VERSION="$(curl --fail --retry 3 -Ls -o /dev/null -w '%{url_effective}' "https://github.com/$1/$2/releases/latest" | sed 's:.*/::')"
   elif [ "$3" = "wget" ]; then
-    effective_url="$(wget --tries=3 --max-redirect=1000 --server-response -O /dev/null "$url" 2>&1 | awk '/Location: /{print $2}' | tail -1)"
-    LATEST_VERSION="$(printf '%s' "$effective_url" | sed 's:.*/::')"
+    LATEST_VERSION="$(wget -qO- https://api.github.com/repos/$1/$2/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')"
   else
     printf '%s\n' "Invalid HTTP client specified."
     return 1
