edfe7d049bbc4b283a609e3618c1b58d441a0663
KyleTryon <Kyle.Tryon@circleci.com>
2023-11-01 14:23:41 -0400

    feat: validate sha256

--- src/scripts/main.sh
+++ src/scripts/main.sh
@@ -77,6 +77,14 @@ print_warn() {
   printf "${yellow}%s${normal}\n" "$1"
 }
 
+# Print an error message
+# $1: The error message to print
+print_error() {
+  red="\033[0;31m"
+  normal="\033[0m"
+  printf "${red}%s${normal}\n" "$1"
+}
+
 print_warn "This is an experimental version of the Slack Orb in Go."
 print_warn "Thank you for trying it out and please provide feedback to us at https://github.com/CircleCI-Public/slack-orb-go/issues"
 
@@ -102,14 +110,14 @@ binary="$orb_bin_dir/$repo_name"
 if [ ! -f "$binary" ]; then
   mkdir -p "$orb_bin_dir"
   if ! determine_http_client; then
-    printf '%s\n' "cURL or wget is required to download the Slack binary."
-    printf '%s\n' "Please install cURL or wget and try again."
+    print_error "cURL or wget is required to download the Slack binary."
+    print_error "Please install cURL or wget and try again."
     exit 1
   fi
   printf '%s\n' "HTTP client: $HTTP_CLIENT."
 
   if ! determine_release_latest_version "$repo_org" "$repo_name" "$HTTP_CLIENT"; then
-    printf '%s\n' "Failed to determine latest version."
+    print_error "Failed to determine latest version."
     exit 1
   fi
   printf '%s\n' "Release's latest version: $LATEST_VERSION."
@@ -121,7 +129,7 @@ if [ ! -f "$binary" ]; then
 
   # TODO: Check the sha256 of the downloaded binary
   if ! download_binary "$binary" "$repo_url" "$HTTP_CLIENT"; then
-    printf '%s\n' "Failed to download $repo_name binary from GitHub."
+    print_error "Failed to download $repo_name binary from GitHub."
     exit 1
   fi
 
@@ -130,9 +138,20 @@ else
   printf '%s\n' "Skipping binary download since it already exists at $binary."
 fi
 
+# Validate the SHA256 checksum if provided
+if [ -n "$SLACK_PARAM_SHA256" ]; then
+  printf '%s\n' "Verifying $binary binary SHA256 checksum..."
+  [ ! "$(which shasum256)" ] && printf '%s\n' "shasum256 is required to verify the SHA256 checksum." && exit 1
+  if ! printf '%s  %s' "$SLACK_PARAM_SHA256" "$binary" | sha256sum -c -; then
+    printf '\033[0;31m%s\n' "Failed to verify $binary binary SHA256 checksum."
+    printf '\033[0m' # Reset text color back to default
+    exit 1
+  fi
+fi
+
 printf '%s\n' "Making $binary binary executable..."
 if ! chmod +x "$binary"; then
-  printf '%s\n' "Failed to make $binary binary executable."
+  print_error "Failed to make $binary binary executable."
   exit 1
 fi
 
