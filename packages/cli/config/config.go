f8a14769bf1bb6be5b94e014ed6fc928c8343222
Eric Ribeiro <ericribeiro@outlook.com.br>
2023-11-16 08:59:28 -0800

    fix: template inline early expansion issue

--- packages/cli/config/config.go
+++ packages/cli/config/config.go
@@ -98,7 +98,6 @@ func (c *Config) expandEnvVariables() error {
 
 		"TemplateVar":    &c.TemplateVar,
 		"TemplatePath":   &c.TemplatePath,
-		"TemplateInline": &c.TemplateInline, // might need to remove this
 		"TemplateName":   &c.TemplateName,
 	}
 
